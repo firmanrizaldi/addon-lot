38f781274d19bed2f74a52c30d7a1451236c6116
Firman Rizaldi Yusup <firmanrizaldiyusup@gmail.com>
2019-11-28 14:17:16 +0700

    ganti lot

--- models/models.py
+++ models/models.py
@@ -4,15 +4,19 @@ from odoo import models, fields, api
 
 class stock_move_lot(models.Model):
     _name = 'stock.move'
-    
     _inherit = 'stock.move'
 
-    lot_id = fields.Many2one(
+    lot = fields.Many2one(
         'stock.production.lot', 'Lot/Serial Number', 
             compute='_create_lot' )
         
     @api.depends('lot_id')
     def _create_lot(self):
-        for record in self:
-            lot_id = record.lot_id.id
-    
+        for x in self:
+            data = self.env('stock.production.lot').search([ 
+            	('product_id','=', x.product_id.id),
+            	('name','=', x.picking_id.origin) 
+            	])
+            x.lot = data.id
+
+
