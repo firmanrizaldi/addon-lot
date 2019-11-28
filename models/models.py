06ce3fbf048ebc4f1dbcfa8f8c929fc33cb67709
Firman Rizaldi Yusup <firmanrizaldiyusup@gmail.com>
2019-11-28 15:46:57 +0700

    lot baru sudah siap

--- models/models.py
+++ models/models.py
@@ -1,22 +1,19 @@
-# -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, fields, models, _
 
-class stock_move_lot(models.Model):
-    _name = 'stock.move'
-    _inherit = 'stock.move'
 
-    lot = fields.Many2one(
-        'stock.production.lot', 'Lot/Serial Number', 
-            compute='_create_lot' )
-        
-    @api.depends('lot_id')
-    def _create_lot(self):
-        for x in self:
-            data = self.env('stock.production.lot').search([ 
-            	('product_id','=', x.product_id.id),
-            	('name','=', x.picking_id.origin) 
-            	])
-            x.lot = data.id
 
+class stockMoveLine(models.Model):
+	_name ='stock.move'
+	_inherit = 'stock.move'
+
+	lot_id = fields.Many2one('stock.production.lot','Lot/Serial number', compute='create_data')
+
+	def create_data(self):
+		for ress in self:
+			data = self.env['stock.production.lot'].search([
+				('product_id','=', ress.product_id.id),
+				('name', '=', ress.picking_id.origin)
+				])
+			ress.lot_id = data.id
 
